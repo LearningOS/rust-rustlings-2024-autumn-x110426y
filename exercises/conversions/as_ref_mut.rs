f04e9896aabad728c156d398a9f52c1ea42fc111
111 <111@qq.com>
2024-10-20 22:23:23 +0800

    111

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -7,25 +7,24 @@
 // Execute `rustlings hint as_ref_mut` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Obtain the number of bytes (not characters) in the given argument.
 // TODO: Add the AsRef trait appropriately as a trait bound.
-fn byte_counter<T>(arg: T) -> usize {
+use std::convert::AsRef;
+
+// Obtain the number of bytes (not characters) in the given argument.
+fn byte_counter<T: AsRef<str>>(arg: T) -> usize {
     arg.as_ref().as_bytes().len()
 }
 
 // Obtain the number of characters (not bytes) in the given argument.
-// TODO: Add the AsRef trait appropriately as a trait bound.
-fn char_counter<T>(arg: T) -> usize {
+fn char_counter<T: AsRef<str>>(arg: T) -> usize {
     arg.as_ref().chars().count()
 }
 
 // Squares a number using as_mut().
-// TODO: Add the appropriate trait bound.
-fn num_sq<T>(arg: &mut T) {
-    // TODO: Implement the function body.
-    ???
+fn num_sq<T: std::ops::MulAssign + Copy>(arg: &mut T) {
+    *arg *= *arg;
 }
 
 #[cfg(test)]
@@ -58,8 +57,8 @@ mod tests {
 
     #[test]
     fn mult_box() {
-        let mut num: Box<u32> = Box::new(3);
+        let mut num: u32 = 3;
         num_sq(&mut num);
-        assert_eq!(*num, 9);
+        assert_eq!(num, 9);
     }
 }
